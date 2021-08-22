250b6f053e245acf606e072c3f6759a333427f89
LucetTin5 <lucettin5@gmail.com>
2021-08-22 13:07:20 +0900

    delete X

--- src/client/scripts/commentSection.js
+++ src/client/scripts/commentSection.js
@@ -15,7 +15,7 @@ const deleteComment = async (event) => {
   const commentId = comment.dataset.id;
   try {
     const { status } = await fetch(`/api/videos/${videoId}/delete-comment`, {
-      method: 'DELETE',
+      method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
